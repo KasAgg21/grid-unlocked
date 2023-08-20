1e37de53c8271acdcf47b6e6adce0eb8e0e09531
KasAgg21 <73023696+KasAgg21@users.noreply.github.com>
2023-08-21 01:02:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -16,23 +16,23 @@ app.get("/", function (req, resp) {
     resp.sendFile(process.cwd() + "/js/public/main.html");
 })
 //-----------------Eth Link -----------------
-const Web3 = require('web3');
+// const Web3 = require('web3');
 
-const infuraUrl = 'https://mainnet.infura.io/v3/08fbbf1f46d24602b703301b084b2c3d'; // Replace with your Infura API key
-const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
+// const infuraUrl = 'https://mainnet.infura.io/v3/08fbbf1f46d24602b703301b084b2c3d'; // Replace with your Infura API key
+// const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
 
-const contractABI = [0xd9145CCE52D386f254917e481eB44e9943F39138]; // Replace with your contract's ABI
-const contractAddress = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4'; // Replace with your contract's address
+// const contractABI = [0xd9145CCE52D386f254917e481eB44e9943F39138]; // Replace with your contract's ABI
+// const contractAddress = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4'; // Replace with your contract's address
 
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+// const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-// Example of calling a contract function
-const result = await contract.methods.someFunction().send({
-  from: '0xe68738600ACb8B821Edf83D9a3AD3eD87C211D34', // Replace with your wallet address
-  gas: 3000000,
-});
+// // Example of calling a contract function
+// const result = contract.methods.someFunction().send({
+//   from: '0xe68738600ACb8B821Edf83D9a3AD3eD87C211D34', // Replace with your wallet address
+//   gas: 3000000,
+// });
 
-console.log('Transaction result:', result);
+//console.log('Transaction result:', result);
 
 
 //-----------------DB Config-----------------
@@ -174,5 +174,15 @@ app.get("/get-all-users",function(req,resp){
         }
         else
         resp.send(err);
+    })
+});
+
+app.get("/get-json", function (req, resp) {
+    var em = req.query.emaili;
+    dbCon.query("select coin from users_cus where uname=?", [em], function (err, resTableJSON) {
+        if (err = null)
+            resp.send(resTableJSON);
+        else
+            resp.send(err);
     })
 });
